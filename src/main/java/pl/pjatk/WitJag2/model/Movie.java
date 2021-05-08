95867ff50fc471bf6f6e8a15a0afac5ec3cad241
s21165 <S21165@pjwstk.edu.pl>
2021-05-08 15:47:18 +0200

    poprawki

--- src/main/java/pl/pjatk/WitJag2/model/Movie.java
+++ src/main/java/pl/pjatk/WitJag2/model/Movie.java
@@ -3,18 +3,40 @@ package pl.pjatk.WitJag2.model;
 
     public class Movie {
         private Long id;
+        private String name;
+        private String category;
 
-        public Movie(long id) {
+        public Movie(Long id, String name, String category) {
             this.id = id;
+            this.name = name;
+            this.category = category;
+        }
 
+        public String getName() {
+            return name;
+        }
+
+        public String getCategory() {
+            return category;
         }
 
         public Long getId() {
             return id;
         }
 
-        public void setMake(Long id) {
+        public void setId(Long id) {
             this.id = id;
         }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+
+
+
     }
 
